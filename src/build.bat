fce4385eccfa07844c810e5951f0d0f7b935f322
debugler <debugler@gmail.com>
2013-04-30 13:32:44 +0200

    build.bat: do not continue build on error.

--- src/build.bat
+++ src/build.bat
@@ -1,3 +1,5 @@
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild debugler.sln /p:VisualStudioVersion=11.0 /m /nologo /t:Build /p:Configuration=Release;platform=x64
+if %errorlevel% neq 0 exit /b %errorlevel% 
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild debugler.sln /p:VisualStudioVersion=11.0 /m /nologo /t:Build /p:Configuration=Release;platform=Win32
+if %errorlevel% neq 0 exit /b %errorlevel% 
 "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" Installer\Installer.vdproj /build Installer
