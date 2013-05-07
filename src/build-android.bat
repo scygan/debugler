d32a5eca644aa7f24b276ee45366bd2a2b9473f5
debugler <debugler@gmail.com>
2013-05-07 14:29:01 +0200

    Integrate Android build into VS (no plugins needed)

--- src/build-android.bat
+++ src/build-android.bat
@@ -1,12 +1,1 @@
-set PATH=%PATH%;C:\Python27
-mkdir ..\build
-mkdir ..\build\android-arm
-cd ..\build\android-arm
-if %errorlevel% neq 0 goto error
-cmake -DCMAKE_TOOLCHAIN_FILE=..\..\src\android.toolchain.cmake ..\..\src\ -G "NMake Makefiles"
-jom
-
-
-:error
-cd ..\..\src
-exit /b 1
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild debugler.sln /p:VisualStudioVersion=11.0 /m /nologo /t:Build /p:Configuration=Release-Android-ARM;platform=Win32
