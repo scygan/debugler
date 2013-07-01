3db5e98d1081d91c9c7db651e285b8a4c77f9147
debugler <debugler@gmail.com>
2013-07-01 19:02:22 +0200

    Build Win32 first, than x64. This is WiX installer requirement.

--- src/build.bat
+++ src/build.bat
@@ -1,3 +1,3 @@
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild debugler.sln /p:VisualStudioVersion=11.0 /m /nologo /t:Build /p:Configuration=Release;platform=x64
-if %errorlevel% neq 0 exit /b %errorlevel% 
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild debugler.sln /p:VisualStudioVersion=11.0 /m /nologo /t:Build /p:Configuration=Release;platform=Win32
+if %errorlevel% neq 0 exit /b %errorlevel% 
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild debugler.sln /p:VisualStudioVersion=11.0 /m /nologo /t:Build /p:Configuration=Release;platform=x64
