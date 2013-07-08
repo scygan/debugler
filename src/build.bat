98efdf2c869ea85f799e6f6bb4514960f0546417
debugler <debugler@gmail.com>
2013-07-08 14:49:23 +0200

    Fixups to versioning.

--- src/build.bat
+++ src/build.bat
@@ -1,4 +1,3 @@
-IF "%DGL_VERSION%"=="" (SET DGL_VERSION=0.0.0)
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild debugler.sln /p:VisualStudioVersion=11.0 /m /nologo /t:Build /p:Configuration=Release;platform=Win32
 if %errorlevel% neq 0 exit /b %errorlevel% 
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild debugler.sln /p:VisualStudioVersion=11.0 /m /nologo /t:Build /p:Configuration=Release;platform=x64
