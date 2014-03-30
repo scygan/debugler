68ad8ae1370d7907a59836e3af96f82f19613795
debugler <debugler@gmail.com>
2014-03-30 15:06:28 +0200

    Reorganize project directory structure a bit: move files from /src/buildtools to /tools

--- tools/set_version.py
+++ tools/set_version.py
@@ -7,18 +7,18 @@ if len(sys.argv) < 2:
     print "not enough arguments"
     exit(1)
 
-outFile = open("../version.inl", "w")
+outFile = open("../src/version.inl", "w")
 outFile.write("\"" + sys.argv[1] + "\"\n")
 outFile.close()
 
 
-shutil.move("../buildtools/vs/auxprojects/Installer/version.wxi", "../buildtools/vs/auxprojects/Installer/version.wxi.old")
-outFile = open("../buildtools/vs/auxprojects/Installer/version.wxi", "w")
-with open("../buildtools/vs/auxprojects/Installer/version.wxi.old", "r") as inFile:
+shutil.move("build/vs/auxprojects/Installer/version.wxi", "build/vs/auxprojects/Installer/version.wxi.old")
+outFile = open("build/vs/auxprojects/Installer/version.wxi", "w")
+with open("build/vs/auxprojects/Installer/version.wxi.old", "r") as inFile:
     for line in inFile:
         if line.startswith("    <?define version="): 
             line = "    <?define version=\"" + sys.argv[1] + "\" ?>\n"
         outFile.write(line)
 inFile.close()
 outFile.close()
-os.remove("../buildtools/vs/auxprojects/Installer/version.wxi.old")
+os.remove("build/vs/auxprojects/Installer/version.wxi.old")
