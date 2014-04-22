d1a25c8e1c05eec9275ccf71d6e78081fd88e2c2
debugler <debugler@gmail.com>
2014-04-22 18:40:10 +0200

    Kill build.bat. Replaced by build.py

--- build.bat
+++
@@ -1,3 +0,0 @@
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild debugler.sln /p:VisualStudioVersion=11.0 /m /nologo /t:Build /p:Configuration=Release-ALL;platform=Win32
-if %errorlevel% neq 0 exit /b %errorlevel% 
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild debugler.sln /p:VisualStudioVersion=11.0 /m /nologo /t:Build /p:Configuration=Release-ALL;platform=x64
